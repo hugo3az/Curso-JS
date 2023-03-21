9ff41942b87830971a934ba41952969071d78e54
hugo3az <123181187+hugo3az@users.noreply.github.com>
2023-03-21 11:15:42 -0300

    Add files via upload

---
+++ Aula 23/Server/main.js
@@ -0,0 +1,122 @@
+const http = require("http");
+const url = require("url");
+
+const fs = require("fs");
+const path = require("path");
+
+const options = {
+  encoding: "utf-8"
+};
+
+const { listTodos, addTodo, updateTodo, deleteTodo, setupCurrentIdTodos } = require("./routers/todos-router");
+const { listCategories, addCategory, updateCategory, deleteCategory, setupCurrentIdCategories } = require("./routers/category-route");
+const { listUser, addUser, updateUser, deleteUser, setupCurrentIdUser } = require("./routers/user-router");
+
+
+let todos = [];
+let categories = [];
+let user = [];
+
+fs.readFile(path.join("db", "user.json"), options, (error, data) => {
+  if (!error) {
+    user = JSON.parse(data);
+    setupCurrentIdUser(user);
+  } else {
+    console.error(error);
+  }
+})
+
+fs.readFile(path.join("db", "category.json"), options, (error, data) => {
+  if (!error) {
+    categories = JSON.parse(data);
+    setupCurrentIdCategories(categories);
+  } else {
+    console.error(error);
+  }
+})
+
+fs.readFile(path.join("db", "todo.json"), options, (error, data) => {
+  if (!error) {
+    todos = JSON.parse(data);
+    setupCurrentIdTodos(todos);
+  } else {
+    console.error(error);
+  }
+})
+
+const writeUSERtoFile = () => {
+  fs.writeFile(path.join("db", "user.json"), JSON.stringify(user), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+const writeTODOtoFile = () => {
+  fs.writeFile(path.join("db", "todo.json"), JSON.stringify(todos), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+const writeCATEGORYtoFile = () => {
+  fs.writeFile(path.join("db", "category.json"), JSON.stringify(categories), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function processRequest(request, response) {
+
+  const reqUrl = url.parse(request.url, true);
+  if (reqUrl.pathname == "/todo") {
+    switch (request.method) {
+      case "GET":
+        listTodos(request, response, reqUrl, todos, categories, user, writeTODOtoFile);
+        break;
+      case "POST":
+        addTodo(request, response, reqUrl, todos, categories, user, writeTODOtoFile);
+        break;
+      case "PUT":
+        updateTodo(request, response, reqUrl, todos, categories, user, writeTODOtoFile);
+        break;
+      case "DELETE":
+        deleteTodo(request, response, reqUrl, todos, categories, user, writeTODOtoFile);
+        break;
+    }
+  } else if (reqUrl.pathname == "/category") {
+    switch (request.method) {
+      case "GET":
+        listCategories(request, response, reqUrl, todos, categories, user, writeCATEGORYtoFile);
+        break;
+      case "POST":
+        addCategory(request, response, reqUrl, todos, categories, user, writeCATEGORYtoFile);
+        break;
+      case "PUT":
+        updateCategory(request, response, reqUrl, todos, categories, user, writeCATEGORYtoFile);
+        break;
+      case "DELETE":
+        deleteCategory(request, response, reqUrl, todos, categories, user, writeCATEGORYtoFile);
+        break;
+    }
+  } else if (reqUrl.pathname == "/user") {
+    switch (request.method) {
+      case "GET":
+        listUser(request, response, reqUrl, todos, categories, user, writeUSERtoFile);
+        break;
+      case "POST":
+        addUser(request, response, reqUrl, todos, categories, user, writeUSERtoFile);
+        break;
+      case "PUT":
+        updateUser(request, response, reqUrl, todos, categories, user, writeUSERtoFile);
+        break;
+      case "DELETE":
+        deleteUser(request, response, reqUrl, todos, categories, user, writeUSERtoFile);
+        break;
+    }
+  }
+}
+const server = http.createServer(processRequest);
+server.listen(3000);
