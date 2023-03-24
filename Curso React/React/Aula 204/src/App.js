13ae7847d6090f5ecfae3a5562cf3cd2cb150cbb
hugo3az <hugo3az@gmail.com>
2023-03-24 17:27:57 -0300

    CURSO REACT ORIGAMI

---
+++ Curso React/React/Aula 204/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+    return <a href="https://drive.google.com/drive/folders/1UrChhs5XB_3xkCZVltmKUFG6d3_bqSQf">Repositorio</a>
+};
+
+export default App;
